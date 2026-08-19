316f463980e12ecad5d67b74d9c80a6de8113940
amorales-2024057 <amorales-2024057@kinal.edu.gt>
2026-08-18 22:04:56 -0600

    fix: Se corrigió el mansaje de expiración del token del usuario, además de crear la vista para el mensaje personalizado

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ModalSesionExpirada } from './shared/modal-sesion-expirada/modal-sesion-expirada';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ModalSesionExpirada],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
--- frontend/src/app/features/login/login.ts
+++ frontend/src/app/features/login/login.ts
@@ -56,4 +56,3 @@ export class Login {
             });
     }
 }
-s
